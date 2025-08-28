700671b7551a0f6b49f3dbad0f3ee67daf393ca9
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-08-28 10:39:33 +0100

    changed name of pipeline, fixed bug missing images

--- pipeline.sh
+++ pipeline.sh
@@ -13,6 +13,11 @@ do
     fi
     for image in brain_t1c brain_t1n brain_t2w brain_t2f
     do 
+        out_path=$OUTPUT_FOLDER/statistics/$tp_id.$image.csv
+        if [ -f $out_path ]; then
+            echo "Statistics already calculated for $tp_id $image"
+            continue
+        fi
         uv run python \
             -m src.extract_statistics \
             --input_folder $INPUT_FOLDER \
@@ -23,12 +28,17 @@ do
 
     for image in brain_t1c brain_t1n brain_t2w brain_t2f
     do 
+        out_path=$OUTPUT_FOLDER/features/$tp_id.$image.csv
+        if [ -f $out_path ]; then
+            echo "Features already calculated for $tp_id $image"
+            continue
+        fi
         uv run python \
             -m src.extract_features \
             --input_folder $INPUT_FOLDER \
             --mask_folder $MASK_FOLDER  \
             --identifier_pattern '(?<=/)[0-9]+(?=/)' \
-            --image_pattern '.*$image.*' \
+            --image_pattern ".*$image.*" \
             --mask_pattern '.*tumor_mask.*' \
             --config_path radiomics/radiomicsConfig.yaml  \
             --output_path $OUTPUT_FOLDER/features/$tp_id.$image.csv \
